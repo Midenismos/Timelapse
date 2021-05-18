e7d8e57e24859f77ede83964d659a756ec90a4e6
snopingas <n.cadre@edu.institutartline.com>
2021-05-18 09:51:32 +0200

    Première version du prototype de Timelapse contenant: - un personnage contrôlable avec vue à la première personne pouvant sauter et ayant la possibilité de prendre les 3 objets temporels - les 3 objets temporels en question sous forme de prefab avec un script fonctionnel (ici slow, accelerate et stop) - un prefab prenant la forme d'un cube rotatif dont la vitesse de rotation dépend des 3 objets temporels - une transition douce entre chaque changement de vitesse grâce au Lerp

---
+++ Timelapse Prototype/Assets/BehaviourPlayer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourPlayer : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera cam;
+    
+    private float speed = 100;
+    private float VerticalMove;
+    private float HorizontalMove;
+    private float velocityX;
+    private float velocityY =0;
+    private float velocityZ;
+
+    private Vector3 moveDirection;
+
+    private float jumpPower = 5f;
+    private float gravity = 9.81f ;
+
+    private float rotationY;
+    private float rotationX;
+    private float minimumX = -60f;
+    private float maximumX = 60f;
+
+
+    private RaycastHit hit;
+
+    public GameObject pickup = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gère la rotation du joueur avec la souris
+        rotationY += Input.GetAxis("Mouse X");
+        rotationX += Input.GetAxis("Mouse Y");
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        cam.transform.localEulerAngles = new Vector3(-rotationX,0, 0);
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+
+        // Gère les inputs de déplacements du joueur
+        HorizontalMove = Input.GetAxis("Horizontal");
+        VerticalMove = Input.GetAxis("Vertical");
+
+        velocityX = HorizontalMove;
+        velocityZ = VerticalMove;
+
+        //Gère la gravité et le saut
+        if (Input.GetKey(KeyCode.Space) == true && Physics.Raycast(transform.position, new Vector3(0, -1), out hit, 1f) == true)
+        {
+            velocityY = Mathf.Sqrt(jumpPower * 2.0f * gravity);
+        }
+        else if (Physics.Raycast(transform.position, new Vector3(0, -1), out hit, 1f) == true)
+        {
+            velocityY = 0;
+        }
+        else
+        {
+            velocityY -= gravity * Time.fixedDeltaTime;
+        }
+
+        moveDirection = (transform.right * velocityX + transform.forward * velocityZ).normalized;
+
+        // Utilise l'item porté
+        if (Input.GetKeyDown("e") == true)
+        {
+            pickup.GetComponent<TimeChanger>().changeTime();
+            Destroy(pickup);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+        //Colle l'item porté par le joueur près de lui
+        pickup.transform.parent = gameObject.transform;
+    }
+
+    //Déplace le joueur
+    public void Move()
+    {
+        rb.velocity = moveDirection * speed * Time.deltaTime;
+        rb.velocity += new Vector3 (rb.velocity.x, velocityY, rb.velocity.z);
+    }
+
+    //Permet au joueur de prendre des items
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            if (Input.GetKey("a"))
+            {
+                pickup = other.gameObject;
+            }
+            Debug.Log("Hey");
+        }
+    }
+}
---
+++ Timelapse Prototype/Assets/TimeChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeChanger : MonoBehaviour
+{
+    //Changer ces variables pour équilibrage
+    [SerializeField]
+    private float Duration = 0.0f;
+
+    [SerializeField]
+    private float NewMultiplier = 0.0f;
+
+    //Ne pas changer ces variables
+    private GameObject TimeManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connecte l'objet au TimeManager
+        TimeManager = GameObject.Find("TimeManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeTime()
+    {
+        //Initialise le timer du TimeManager
+        TimeManager.GetComponent<TimeManager>().timer = Duration;
+        //Change le multiplier de vitesse
+        TimeManager.GetComponent<TimeManager>().StartTimeChange(NewMultiplier);
+    }
+}
