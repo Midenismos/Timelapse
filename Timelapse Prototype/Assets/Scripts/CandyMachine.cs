5d755f3679248e7f10af41dc299748d759f2d36c
snopingas <n.cadre@edu.institutartline.com>
2021-05-26 15:29:21 +0200

    - Changement de la taille du joueur, de sa vitesse de déplacement et sa hauteur de saut. - Ajout de "machines à bonbon" distribuant les nouritures temporelles et pouvant être paramétrés.

---
+++ Timelapse Prototype/Assets/Scripts/CandyMachine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CandyMachine : MonoBehaviour
+{
+    public GameObject timeFood;
+    public float duration;
+    public GameObject spawnPosition;
+    private Vector3 pos;
+    public ParticleSystem particle;
+    public GameObject text;
+    public string type;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = spawnPosition.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.GetComponent<TextMeshPro>().text = type + " " + duration;
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown("a"))
+            {
+                GameObject prefab = Instantiate(timeFood);
+                prefab.transform.position = pos;
+                prefab.GetComponent<TimeChanger>().timeChange.duration = duration;
+                prefab.GetComponentInChildren<TextMeshPro>().text = type + " " + duration;
+                particle.Play();
+
+            }
+        }
+    }
+}
