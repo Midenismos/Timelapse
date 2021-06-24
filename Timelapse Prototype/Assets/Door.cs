f6012959714a3d9ee2cc2f5b34b025b33f76e03d
Jeanne <j.d.m.aubry@gmail.com>
2021-06-24 20:32:31 +0200

    porte (sans effet du rewind) + trigger player et spawner du cube

---
+++ Timelapse Prototype/Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Rewindable, ITimeStoppable
+{
+    [SerializeField] private Animator animator = null;
+    public void StartTimeStop()
+    {
+        animator.enabled = false;
+    }
+
+    public void EndTimeStop()
+    {
+        animator.enabled = true;
+    }
+
+    public override void StartRewind()
+    {
+        base.StartRewind();
+    }
+
+    public override void EndRewind()
+    {
+        base.EndRewind();
+    }
+}
